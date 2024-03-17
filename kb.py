ea3379d009e9bf0f6b78fd0ad7d7f223eb3de8ad
savelovg23 <160651165+savelovg23@users.noreply.github.com>
2024-03-17 23:34:19 +0300

    Delete kb.py

--- kb.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-menu = [
-    [InlineKeyboardButton(text="Список групп", callback_data="group_list"),
-    InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
-]
-menu = InlineKeyboardMarkup(inline_keyboard=menu)
-exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
-iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
